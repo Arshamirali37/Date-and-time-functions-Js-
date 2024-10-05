8ad3dd14f35423523bf8a8cd80f6460336e5c72c
ARSHamirali <arshamirali604@gmail.com>
2024-10-05 10:39:39 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+// DATE:3/10/2024 
+
+//                  {RANDOM PASSWORD GENERATOR}
+// var randomvalue="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*"
+
+// var randompassword=""
+// for (var i=0; i< 10 ; i++){
+// var randomnumber=Math.round(Math.random()*randomvalue.length)
+// randompassword += randomvalue[randomnumber]}
+// console.log(randompassword )
+
+
+//                  { DATE AND TIME }
+// var date = new Date()
+// console.log(date)
+// console.log(date.toString().slice(0,10))
+ 
+
+//                { AGE CALCULATOR } 
+// var date = new Date()
+// var userinput=+prompt("ENTER YOUR BIRTH YEAR")
+// var currentyear=date.getFullYear()
+// var age =currentyear-userinput
+// console.log(age)
+
+
+//                  { RAMDAN UPCOMING MONTHS GENERATOR }
+// var date=new Date()
+
+// var firstramdan=new Date("March 1,2025").getTime()
+
+//  var current=new Date().getTime()
+   
+//  var diff = firstramdan - current;
+ 
+//  var Month=diff / (1000*60*60*24*30) 
+
+//  console.log("MONTHS",Math.round(Month))
+
+
+//                  { RAMDAN UPCOMING DAYS GENERATOR }
+//  var days=new Date("March 1, 2025").getTime()
+  
+//  var currentDays=new Date().getTime()
+
+//  var diff2=days-currentDays
+  
+//  var daysleft=diff2/(1000*60*60*24)
+
+//  console.log("DAYS",Math.round(daysleft));
+ 
+
+//                            {FUNCTION}
+//      function DATE(){
+//      var date = new Date().getFullYear();
+//      console.log(date)}
+//       DATE() // Function Calling Method..
+ 
+
+
+
+
+
+
